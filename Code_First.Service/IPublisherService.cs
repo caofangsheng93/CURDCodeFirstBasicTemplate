e8e54d74aa00a144ec42b66263f2e3058c45e435
caofangsheng93 <fangsheng93@163.com>
2016-06-01 18:19:29 +0800

    ..

--- Code_First.Service/IPublisherService.cs
+++ Code_First.Service/IPublisherService.cs
@@ -1,9 +1,5 @@
 ﻿using CodeFirst.Entities.Entities;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Code_First.Service
 {
