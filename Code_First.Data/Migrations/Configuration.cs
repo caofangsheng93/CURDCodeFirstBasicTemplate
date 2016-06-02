3a5e74a0f417808e496b1d4f930c794559229b1b
caofangsheng93 <fangsheng93@163.com>
2016-06-02 17:22:20 +0800

    ...

---
+++ Code_First.Data/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Code_First.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Code_First.Data.DbContextClass.Code_First_DbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;  
+        }
+
+        protected override void Seed(Code_First.Data.DbContextClass.Code_First_DbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
