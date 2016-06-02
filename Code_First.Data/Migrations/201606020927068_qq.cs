0739447e058a1c0a0c40377dfeb1448b5881c96e
caofangsheng93 <fangsheng93@163.com>
2016-06-02 17:31:06 +0800

    ....

---
+++ Code_First.Data/Migrations/201606020927068_qq.cs
@@ -0,0 +1,16 @@
+namespace Code_First.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class qq : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Code_First.Data/Migrations/Configuration.cs
+++ Code_First.Data/Migrations/Configuration.cs
@@ -1,11 +1,12 @@
 namespace Code_First.Data.Migrations
 {
+    using Code_First.Data.DbContextClass;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Code_First.Data.DbContextClass.Code_First_DbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Code_First_DbContext>
     {
         public Configuration()
         {
@@ -13,7 +14,7 @@ namespace Code_First.Data.Migrations
             AutomaticMigrationDataLossAllowed = true;  
         }
 
-        protected override void Seed(Code_First.Data.DbContextClass.Code_First_DbContext context)
+        protected override void Seed(Code_First_DbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
