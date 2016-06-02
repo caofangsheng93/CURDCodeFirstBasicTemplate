57e403c19efc6f1b8fa29d4d41905e6181647672
caofangsheng93 <fangsheng93@163.com>
2016-06-02 16:47:25 +0800

    BookService

---
+++ Code_First.Service/BookService.cs
@@ -0,0 +1,55 @@
+﻿using Code_First.Data.Repository;
+using CodeFirst.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First.Service
+{
+   public class BookService:IBookService
+    {
+       private Repository<Book> _bookRepository;
+
+       public BookService(Repository<Book> bookRepository)
+       {
+           this._bookRepository = bookRepository;
+       }
+        public IQueryable<Book> GetAllBooks()
+        {
+            return _bookRepository.GetAll().AsQueryable();
+            //throw new NotImplementedException();
+        }
+
+        public Book GetBookById(int id)
+        {
+            return _bookRepository.GetById(id);
+            //throw new NotImplementedException();
+        }
+
+        public void InsertBook(Book model)
+        {
+            _bookRepository.Insert(model);
+            //throw new NotImplementedException();
+        }
+
+        public void UpdateBook(Book model)
+        {
+            _bookRepository.Update(model);
+            //throw new NotImplementedException();
+        }
+
+        public void DeleteBook(int id)
+        {
+            _bookRepository.Delete(id);
+            //throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            _bookRepository.Save();
+           // throw new NotImplementedException();
+        }
+    }
+}
---
+++ Code_First.Service/IBookService.cs
@@ -0,0 +1,24 @@
+﻿using CodeFirst.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_First.Service
+{
+   public interface IBookService
+    {
+       IQueryable<Book> GetAllBooks();
+
+       Book GetBookById(int id);
+
+       void InsertBook(Book model);
+
+       void UpdateBook(Book model);
+
+       void DeleteBook(int id);
+
+       void Save();
+    }
+}
